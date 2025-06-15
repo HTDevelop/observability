32351aabda51fb558933bef3107a62c4137c21a2
root <root@ip-172-31-39-255.ap-northeast-1.compute.internal>
2025-06-15 02:04:39 +0000

    公開用に編集

--- app/k6.js
+++ app/k6.js
@@ -3,11 +3,11 @@ import { check } from 'k6';
 
 export const options = {
   vus: 5,
-  duration: '15s',
+  duration: '5s',
 };
 
 export default function () {
-  const res = http.get('https://www.higuchi-dev.jp/');
+  const res = http.get('https://test.k6.io/');
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
